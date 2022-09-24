70123a7b3738d2b227755fe563c9330b350d75de
Miguel Martín-Maestro López <miguelmml1993@gmail.com>
2022-09-24 14:20:24 +0200

    Add swagger documentation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const apicache = require('apicache')
+const v1WorkoutRouter = require('./v1/routes/workoutRoutes')
+const { swaggerDocs: V1SwaggerDocs } = require('./v1/swagger')
 
 const app = express()
-const v1WorkoutRouter = require('./v1/routes/workoutRoutes')
-
 const PORT = process.env.PORT || 3000
 const cache = apicache.middleware
 
@@ -14,4 +14,5 @@ app.use('/api/v1/workouts', v1WorkoutRouter)
 
 app.listen(PORT, () => {
   console.log(`🚀 Server listening on port: ${PORT}`)
+  V1SwaggerDocs(app, PORT)
 })
